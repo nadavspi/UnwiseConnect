6c5bfc6054d7ad6b116c790f12dd9b05e2c542b8
Nadav Spiegelman <me@nadav.name>
2017-03-08 19:48:27 -0500

    Only save tickets in redux-storage

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import * as storage from 'redux-storage'
 import App from './components';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ActionTypes } from './config/constants';
 import createEngine from 'redux-storage-engine-indexed-db';
+import filter from 'redux-storage-decorator-filter';
 import rootReducer from './reducers/';
 import thunk from 'redux-thunk';
+import { ActionTypes } from './config/constants';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 
 const reducer = storage.reducer(rootReducer);
-const engine = createEngine('tickets');
+const engine = filter(createEngine('tickets'), ['tickets']);
 
 const store = createStore(
   reducer,
