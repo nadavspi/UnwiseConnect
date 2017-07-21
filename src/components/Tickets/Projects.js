dd2cb266f6c5d9d9d22248c4f96ab214f4ece2ee
Leland Clemmons <leland.clemmons@gmail.com>
2017-07-20 20:43:46 -0400

    Move projects dropdown to Bootstrap list-group

--- src/components/Tickets/Projects.js
+++ src/components/Tickets/Projects.js
@@ -2,26 +2,24 @@ import React from 'react';
 
 const Projects = props => {
   return (
-    <ul className="projects">
+    <div className="projects list-group">
       {Object.keys(props.projects).map(projectId => {
         const ticket = props.projects[projectId][0];
         return (
-          <li key={projectId}>
-            {ticket.company.name} &mdash;
-            <button
-              type="button"
-              className="btn-link"
-              onClick={props.searchProject.bind(this, {
-                company: ticket.company.name,
-                project: ticket.project.name,
-              })}
-            >
-              {ticket.project.name}
-            </button>
-          </li>
+          <button
+            key={projectId}
+            type="button"
+            className="list-group-item"
+            onClick={props.searchProject.bind(this, {
+              company: ticket.company.name,
+              project: ticket.project.name,
+            })}
+          >
+            <strong>{ticket.company.name}</strong> &mdash; {ticket.project.name}
+          </button>
         );
       })}
-    </ul>
+    </div>
   );
 };
 
