10202f3f02cef695f6263b0a2ca474cc2ef70026
Nadav Spiegelman <me@nadav.name>
2017-08-11 14:00:20 -0400

    Add other field types

--- src/components/Dispatch/Fields/index.js
+++ src/components/Dispatch/Fields/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 const renderField = (field, onChange) => {
   switch (field.type) {
     case 'text': 
+    case 'number':
       return (
         <span>
           <label 
@@ -15,11 +16,57 @@ const renderField = (field, onChange) => {
             id={field.id}
             onChange={onChange}
             required={field.required} 
-            type="text"
+            type={field.type}
             value={field.value}
           />
         </span>
       );
+
+    case 'boolean':
+      return (
+        <span>
+          <input 
+            checked={field.value}
+            onChange={onChange}
+            type="checkbox"
+          />
+          {' '}
+          <label 
+            htmlFor={field.id}
+          >
+            {field.id}
+          </label>
+        </span>
+      );
+
+    case 'select':
+      return (
+        <span>
+          <label 
+            htmlFor={field.id}
+            style={{ display: 'block' }}
+          >
+            {field.id}
+          </label>
+        
+          <select 
+            id={field.id}
+            onChange={onChange}
+            required={field.required}
+            value={field.value}
+          >
+            {field.values.map(option => (
+              <option 
+                value={option}
+                key={option}
+              >
+                {option}
+              </option>
+            ))}
+          </select>
+        </span>
+      );
+
     default:
       return;
   }
@@ -29,7 +76,7 @@ const Fields = props => {
   return (
     <form className="dispatch-form">
       {props.fields.map(field => (
-        <p>
+        <p key={field.id}>
           {renderField(field, props.onChange)}
         </p>
       ))}
