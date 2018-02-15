bce7fa543f57d3e4a5a4954c08787e6e98569f2a
Todd Christensen <tchristensen@somethingdigital.com>
2018-02-14 18:25:10 -0800

    Make SearchColumns more complex component.
    No actual functional changes yet, just for indentation/etc.

--- src/components/Tickets/SearchColumns.js
+++ src/components/Tickets/SearchColumns.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
-const SearchColumns = ({ columns, query, onChange }) => {
-  const onQueryChange = (event) => {
-    onChange({
-      ...query,
-      [event.target.name]: event.target.value
-    });
-  };
+class SearchColumns extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <tr>
-      {columns.map((column, i) => (
-        <th key={`${column.property || i}-column-filter`} className="column-filter">
-          {column && column.property ?
-            <input
-              onChange={onQueryChange}
-              className="column-filter-input"
-              name={column.property}
-              placeholder={column.filterPlaceholder || ''}
-              value={query[column.property] || ''}
-            />
-          : ''}
-        </th>
-      ))}
-    </tr>
-  );
+  render() {
+    const onQueryChange = (event) => {
+      this.props.onChange({
+        ...this.props.query,
+        [event.target.name]: event.target.value
+      });
+    };
+
+    return (
+      <tr>
+        {this.props.columns.map((column, i) => (
+          <th key={`${column.property || i}-column-filter`} className="column-filter">
+            {column && column.property ?
+              <input
+                onChange={onQueryChange}
+                className="column-filter-input"
+                name={column.property}
+                placeholder={column.filterPlaceholder || ''}
+                value={this.props.query[column.property] || ''}
+              />
+            : ''}
+          </th>
+        ))}
+      </tr>
+    );
+  }
 };
+
 SearchColumns.propTypes = {
   columns: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
   onChange: React.PropTypes.func.isRequired,
