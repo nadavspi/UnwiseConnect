b7662295395438d8a7f326e0c14eebca2fdc997a
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-06-28 13:23:47 -0400

    Reattempt at expanding Item data structure

--- src/components/Budgets/Item/Form.js
+++ src/components/Budgets/Item/Form.js
@@ -96,92 +96,79 @@ export default class ItemForm extends Component {
     //   }
     // }
     
-    // const fields = [
-    //   {
-    //     name: 'summary',
-    //     label: 'Summary',
-    //     type: 'text',
-    //     required: true,
-    //   },
-    //   {
-    //     name: 'phase',
-    //     label: 'Phase',
-    //     type: 'text',
-    //     required: true,
-    //   },
-    //   {
-    //     name: 'feature',
-    //     label: 'Feature',
-    //     type: 'text',
-    //   },
-    //   {
-    //     name: 'column',
-    //     label: 'Team',
-    //     type: 'text',
-    //     group: 'budgetHours',
-    //   },
-    //   {
-    //     name: 'value',
-    //     label: 'Hours',
-    //     type: 'number',
-    //     group: 'budgetHours',
-    //   },
-    //   {
-    //     name: 'workplan',
-    //     label: 'Workplan description',
-    //     type: 'text',
-    //     group: 'descriptions',
-    //     required: true,
-    //   },
-    //   {
-    //     name: 'budget',
-    //     label: 'Budget description',
-    //     type: 'text',
-    //     group: 'descriptions',
-    //   },
-    //   {
-    //     name: 'assumptions',
-    //     label: 'Assumptions',
-    //     type: 'text',
-    //     group: 'descriptions',
-    //   },
-    //   {
-    //     name: 'exclusions',
-    //     label: 'Exclusions',
-    //     type: 'text',
-    //     group: 'descriptions',
-    //   },
-    //   {
-    //     name: 'tags',
-    //     label: 'Tags',
-    //     type: 'text',
-    //     required: true,
-    //   },
-    // ];
+    const fields = [
+      {
+        name: 'summary',
+        label: 'Summary',
+        type: 'text',
+        required: true,
+      },
+      {
+        name: 'phase',
+        label: 'Phase',
+        type: 'text',
+        required: true,
+      },
+      {
+        name: 'feature',
+        label: 'Feature',
+        type: 'text',
+      },
+      {
+        name: 'column',
+        label: 'Team',
+        type: 'text',
+        group: 'budgetHours',
+      },
+      {
+        name: 'value',
+        label: 'Hours',
+        type: 'number',
+        group: 'budgetHours',
+      },
+      {
+        name: 'workplan',
+        label: 'Workplan description',
+        type: 'text',
+        group: 'descriptions',
+        required: true,
+      },
+      {
+        name: 'budget',
+        label: 'Budget description',
+        type: 'text',
+        group: 'descriptions',
+      },
+      {
+        name: 'assumptions',
+        label: 'Assumptions',
+        type: 'text',
+        group: 'descriptions',
+      },
+      {
+        name: 'exclusions',
+        label: 'Exclusions',
+        type: 'text',
+        group: 'descriptions',
+      },
+      {
+        name: 'tags',
+        label: 'Tags',
+        type: 'text',
+        required: true,
+      },
+    ];
     
     // let field, group;
     // let groups = [];
 
     // for(let i = 0; i < fields.length; i++){
     //   field = fields[i];
-      
     //   if(field.group != null){
-    //     console.log('Field Name: ', field.name);
-    //     console.log('Field Value: ', item[field.name]);
-    //     group = field.group;
-    //     if(groups[group] == null){
-    //       console.log('new group', groups[group]);
-    //       groups[group] = {
-    //         [field.name]: item[field.name]
-    //       };
-    //     } else {
-    //       console.log('existing group', groups[group]);
-    //       groups[group] = {
-    //         ...groups[group], 
-    //         [field.name]: item[field.name],
-    //       };
-    //     }
-    //     groups[field.group][field.name] = item[name];
+    //     groups[field.group] = {
+    //       groups[field.group],
+    //       [field.name]: item[field.name],
+    //     }; 
     //   }
     // }
     // console.log('Groups: ', groups);
