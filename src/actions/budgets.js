2bcd1e9d58c70df427627b86dd653112553d6ccc
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-07-05 16:22:04 -0400

    Moved delete item to Redux Store

--- src/actions/budgets.js
+++ src/actions/budgets.js
@@ -3,7 +3,7 @@ import { ActionTypes } from '../config/constants';
 export const subscribe = (payload) => {
 	return {
 		type: ActionTypes.BUDGETS_SUBSCRIBE,
-		
+
 	}
 }
 
@@ -11,5 +11,12 @@ export const addItem = (item) => {
 	return {
 		type: ActionTypes.BUDGETS_ADD_ITEM,
 		item,
+	}
+}
+
+export const removeItem = (itemId) => {
+	return {
+		type: ActionTypes.BUDGETS_REMOVE_ITEM,
+		itemId,
 	}
 }
