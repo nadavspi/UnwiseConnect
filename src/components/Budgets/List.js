3a1dfd6588142fe13e612eeac45c3d0781b9abf9
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-07-05 17:53:13 -0400

    List view uses searchtabular instead of isVisible flag

--- src/components/Budgets/List.js
+++ src/components/Budgets/List.js
@@ -1,11 +1,30 @@
+import * as search from 'searchtabular';
 import React, { Component } from 'react';
 import Item from './Item';
 import SearchBar from './Search';
+import Pagination from '../Tickets/Pagination';
+import { compose } from 'redux';
+import { multiInfix } from '../../helpers/utils';
 
 class List extends Component {
 
 	      
 	render() {
+    const { query, rows } = this.props;
+    const columns = this.props.fields.map((field) => field={
+      property: field.name,      
+      header: {
+        label: field.label,
+      },
+      filterType:field.filterType,
+    });
+
+    const searchExecutor = search.multipleColumns({ 
+      columns, 
+      query, 
+      strategy: multiInfix });
+    const visibleItems = compose(searchExecutor)(rows);
+
 		return (
 			<div>
 			 <h2> List View </h2>
@@ -14,8 +33,7 @@ class List extends Component {
           fields={this.props.fields}
           onFilter={this.props.onFilter}
         />
-        {this.props.items.map(item => 
-          item.isVisible && (
+        {visibleItems.map(item => 
           <Item 
             item={item}
             fields={this.props.fields}
@@ -23,7 +41,7 @@ class List extends Component {
             onDelete={this.props.onDelete}
             key={item.id}
           />
-        ))}
+        )}
       </div>
 		);
 	}
