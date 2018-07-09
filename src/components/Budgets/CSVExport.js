65427f1f77b565c91e9ba5d038eb1663fc833efe
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-07-09 14:32:11 -0400

    Move reformatColumns to helpers/reformat

--- src/components/Budgets/CSVExport.js
+++ src/components/Budgets/CSVExport.js
@@ -2,6 +2,7 @@ import flatten from 'flat';
 import jsonexport from 'jsonexport';
 import React, { Component } from 'react';
 import { CSVLink } from 'react-csv';
+import { reformatColumns } from '../../helpers/reformat';
 
 class CSVExport extends Component {
 	constructor(){
@@ -25,61 +26,7 @@ class CSVExport extends Component {
 	}
 
 	exportFile() {
-		return this.reformatColumns(this.props.visibleItems);
-	}
-
-	reformatColumns(items) {
-		const flatList = items.map((item) => flatten(item, { maxDepth: 2 }));
-		
-		// convert columns
-    const reformattedList = flatList.map((item) => ({
-      ...item,
-      // Put the hours in the right column
-      [item['budgetHours.column']]: item['budgetHours.value'],
-      total: item['budgetHours.value'],
-    }));
-
-		let concatObj = {};
-
-    const itemsByFeature = reformattedList.reduce((features, item) => {
-      features[item.feature] = [
-        ...features[item.feature] || [],
-        item,
-      ];
-
-      return features;
-    }, {});
-
-    console.log({ itemsByFeature, reformattedList });
-
-		// combine on feature
-		reformattedList.map((item) => {
-      // Feature isn't already in the object
-			if (typeof concatObj[item.feature] === 'undefined') {
-				concatObj[item.feature] = item;
-			} else {
-        // Add to existing feature
-				for (const property in item) {
-					if(Array.isArray(item[property])) {
-            concatObj[item.feature][property] = [
-              ...concatObj[item.feature][property],
-              ...item[property],
-            ];
-					} else if(typeof item[property] === 'number') {
-            // Sum the numbers
-						concatObj[item.feature][property] = concatObj[item.feature][property] + item[property] || item[property];
-					} 
-				}
-			}
-		});
-
-		// convert concatObj from properties to array elements
-		let concatList = [];
-		for (const element in concatObj) {
-			concatList = [...concatList, concatObj[element]];
-		}
-
-		return concatList;
+		return reformatColumns(this.props.visibleItems);
 	}
 
 	render() {
