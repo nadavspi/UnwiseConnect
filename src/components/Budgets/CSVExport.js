0219d76c140b31ae6707258ff5ac16fe9c4d0ebe
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-07-09 12:15:53 -0400

    Add CSV export to browser
    Previously, clicking csv export would print a csv
    formatted string to console. Now when clicking export
    a csv file containing the data currently in the table
    will be downloaded through the browser.

--- src/components/Budgets/CSVExport.js
+++ src/components/Budgets/CSVExport.js
@@ -1,6 +1,7 @@
 import flatten from 'flat';
 import jsonexport from 'jsonexport';
 import React, { Component } from 'react';
+import { CSVLink } from 'react-csv';
 
 class CSVExport extends Component {
 	constructor(){
@@ -9,7 +10,7 @@ class CSVExport extends Component {
 		this.exportFile = this.exportFile.bind(this);
 	}
 
-	convertFileType(data){
+	convertFileType(data) {
 
 		const options = {
 			headers: this.props.columns.map((column) => (column.value)),
@@ -20,17 +21,14 @@ class CSVExport extends Component {
 			if(err) {
 				return console.log(err);
 			}
-			this.exportToBroswer(csv);
+			return csv;
 		});
-
 	}
 
 	exportFile() {
-		this.convertFileType(this.reformatColumns(this.props.filterItems(this.props.items,this.props.query)));
-	}
-
-	exportToBroswer(csv) {
-		console.log(csv);
+		const data = this.reformatColumns(this.props.filterItems(this.props.items,this.props.query));
+		console.log(data);
+		return data;
 	}
 
 	reformatColumns(items) {
@@ -81,57 +79,76 @@ class CSVExport extends Component {
 	}
 
 	render() {
+
 		return (
-			<button 
-				onClick={this.exportFile}
-				className="btn btn-primary">
-				Export
-			</button>
+			<div>
+				<CSVLink
+					data={this.exportFile()}
+					headers={this.props.columns}
+					filename={'Budget.csv'}
+					className="btn btn-primary">
+					Export to CSV
+				</CSVLink>
+			</div>
 		);
 	}
 }
 
 CSVExport.defaultProps = {
 	columns: [{
+		key:'feature',		
 		value:'feature',
 		label:'Page/Feature',
 	},{
+		key:'t&m',
 		value:'t&m',
 		label:'T&M',
 	},{
+		key:'Discovery',
 		value:'Discovery',
 		label:'Disc',
 	},{
+		key:'Design',
 		value:'Design',
 		label:'Design',
 	},{
+		key:'Dev',
 		value:'Dev',
 		label:'Dev',
 	},{
+		key:'Testing',
 		value:'Testing',
 		label:'Testing',
 	},{
+		key:'Remediation',
 		value:'Remediation',
 		label:'Remediation',
 	},{
+		key:'Deploy',
 		value:'Deploy',
 		label:'Deploy',
 	},{
+		key:'PM',
 		value:'PM',
 		label:'PM',
 	},{
+		key:'total',
 		value:'total',
 		label:'Total',
 	},{
+		key:'descriptions.budget',
 		value:'descriptions.budget',
 		label:'Description',
 	},{
+		key:'descriptions.assumptions',
 		value:'descriptions.assumptions',
 		label:'Assumptions',
 	},{
+		key:'descriptions.clientResponsibilities',
 		value:'descriptions.clientResponsibilities',
 		label:'Client Responsibilities',
 	},{
+		key:'descriptions.exclusions',
 		value:'descriptions.exclusions',
 		label:'Exclusions',
 	},
