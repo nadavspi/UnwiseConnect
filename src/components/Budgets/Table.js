1ef62dd2f99bef369cf18e22220e5102771bdc1a
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-07-12 15:54:14 -0400

    Create test for memoization
    Created a test set up to demonstrate a possible
    way to improve performance by reducing the
    number of times a search query filters the list
    of items to strictly events that would affect
    the displayed list. (e.g. Add, Delete, Edit)

--- src/components/Budgets/Table.js
+++ src/components/Budgets/Table.js
@@ -1,8 +1,16 @@
+import memoize from 'memoize-one';
 import CSVExport from './CSVExport';
 import MultiSearch from './MultiSearch';
 import React, { Component } from 'react';
 import TicketTable from '../Tickets/Table';
   
+const filterfun = (len) => {
+  console.log('run search query');
+  return len;
+};
+
+const filter = memoize(filterfun);
+
 class Table extends Component {
   
   onCustomFilter(property){
@@ -15,7 +23,13 @@ class Table extends Component {
     }
   }
 
+  log(value) {
+    console.log('RENDERED');
+  }
+
   render() {
+    const value = filter(...this.props.visibleItems);
+
     const columns = this.props.fields.map((field) => {
       const column = {
         property: field.name,
@@ -38,14 +52,15 @@ class Table extends Component {
 
     return (
       <div>
+        {this.log(value)}
         <TicketTable
+          columns={columns}
           id="table-search-items"
           query={this.props.query}
           search={this.props.search}
           tickets={this.props.items}
           toggleColumn={this.props.toggleColumn}
           userColumns={userColumns}
-          columns={columns}
         />
         <CSVExport 
           items={this.props.items}
