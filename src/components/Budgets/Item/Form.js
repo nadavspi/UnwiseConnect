adb2d2b580ff9c5a64382c8cab4ca55719999782
Jayson DeMarchi <jdemarchi@somethingdigital.com>
2018-07-13 11:01:40 -0400

    Add interactive check on fields
    With the inclusion of the edit field, the form
    was displaying the option to edit the edit field.
    This flag will help hide the interactive fields.

--- src/components/Budgets/Item/Form.js
+++ src/components/Budgets/Item/Form.js
@@ -45,21 +45,17 @@ export default class ItemForm extends Component {
     const { fields } = this.props;
     const submitBtnLabel = this.props.isEditing ? 'Save Edit' : 'Add Item';
 
-    return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <div className="input-group input-group-sm">            
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+					<div>						
             {fields.map((field) => (
-              <div key={field.name}>
-                <label htmlFor={field.name}>{field.label}</label>
-                <input 
-                  ref={field.name}
-                  onChange={e => this.onChange(field.name, e.target.value)}
-                  type={field.type}
-                  value={this.state.item[field.name]}
-                  required={field.required}
-                />
-              </div>
+              !field.isInteractive && (
+                <div key={field.name}>
+                  <label htmlFor={field.name}>{field.label}</label>
+                  {this.inputFormat(field)}
+                </div>
+              )
             ))}
             <button type="submit" className="btn btn-primary">{submitBtnLabel}</button>
             {this.props.isEditing && (
