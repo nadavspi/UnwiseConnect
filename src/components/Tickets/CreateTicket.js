4633e118c711e16ebae2653477eee4bd1623f5d9
Kyle Leighton <kyle.leighton1@gmail.com>
2020-12-31 11:16:49 -0500

    Start adding form

--- src/components/Tickets/CreateTicket.js
+++ src/components/Tickets/CreateTicket.js
@@ -1,13 +1,20 @@
 import React, { PureComponent } from 'react';
 
 class CreateTicket extends PureComponent {
+  state = {
+    expanded: false,
+  }
+
   render() {
+    
     return (
       <React.Fragment>
         <button className="btn btn-default btn-lg" type="button" aria-label="add">＋ Create Ticket</button>
-        <div>
-          
-        </div>
+        {this.state.expanded && (
+          <form>
+            
+          </form>
+        )}
       </React.Fragment>
     )
   }
