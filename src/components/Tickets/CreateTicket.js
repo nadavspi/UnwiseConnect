963cdfe5e50679d2e49acd351397d76d71c7e238
Kyle Leighton <kyle.leighton1@gmail.com>
2021-01-05 18:55:50 -0500

    Remove unnecessary React.Fragments and add a class to a button

--- src/components/Tickets/CreateTicket.js
+++ src/components/Tickets/CreateTicket.js
@@ -146,7 +146,6 @@ class CreateTicket extends PureComponent {
           <button
             className="btn btn-default btn-md btn-expand"
             type="button"
-            aria-label="add"
             onClick={() => this.expandAddTicketForm()}
           >
             {this.state.expanded ? '—' : '＋'} Create Ticket
@@ -160,115 +159,111 @@ class CreateTicket extends PureComponent {
           shouldCloseOnOverlayClick={true}
         >
           <form>
-              <React.Fragment>
-                <div className="autocomplete-field">
-                  <label htmlFor="projects">Project</label><br></br>
-                  <input
-                    id="projects"
-                    className="form-control"
-                    disabled="disabled"
-                    value={`${this.props.selectedProject['company.name']} — ${this.props.selectedProject['project.name']}`}
-                  />
-                </div>
-                <div className="autocomplete-field">
-                    <React.Fragment>
-                      <label htmlFor="phases">Phase</label><br></br>
-                      <Autocomplete
-                        id="phases"
-                        items={this.state.phases}
-                        getItemValue={item => item.path}
-                        shouldItemRender={(item, value) => item.path.toLowerCase().indexOf(value.toLowerCase()) > -1}
-                        renderItem={(item, highlighted) =>
-                          <div key={`${item.phaseId}-${item.ticketId}`}>
-                            {item.path}
-                          </div>
-                        }
-                        value={this.state.phaseValue}
-                        inputProps={{ className: "autocomplete-input form-control" }}
-                        onChange={e => this.setState({ phaseValue: e.target.value })}
-                        onSelect={value => {
-                          this.setState({
-                            phaseValue: value,
-                            selectedPhase: this.state.phases.filter(phase => phase.path === value),
-                          })
-                        }}
-                      />
-                    </React.Fragment>
-                </div>
-                  {this.state.phaseValue && (
-                    <div>
-                      <label htmlFor="summary">Summary</label>
-                      <input
-                        className="form-control"
-                        type="text"
-                        id="summary"
-                        onChange={(e) => this.setState({ summary: e.target.value })}
-                        required
-                        autoComplete="off"
-                      ></input>
-                    </div>
-                  )}
-                  {this.state.summary && (
-                    <div>
-                      <label htmlFor="budget-hours">Budget Hours</label>
-                      <input
-                        type="number"
-                        id="budget-hours"
-                        className="form-control"
-                        onChange={(e) => this.setState({ budget: e.target.value })}
-                        required
-                        min="0"
-                        step="0.25"
-                        placeholder="1"
-                        autoComplete="off"
-                      >
-                      </input>
-                      {this.state.budget > 10 && (<p>Warning: This is a higher than normal budget</p>)}
-                    </div>
-                  )}
-                  {this.state.budget > 0 && (
-                    <div>
-                      <label htmlFor="initial-description">Description</label>
-                      <textarea
-                        id="initial-description"
-                        rows="4"
-                        cols="50"
-                        className="form-control"
-                        placeholder="This is optional"
-                        onChange={(e) => e.target.value.length > 5 && this.setState({ initialDescription: e.target.value })}
-                      >
-                      </textarea>
-                    </div>
-                  )}
-                  {(this.state.summary && this.state.budget) && (
-                    <button
-                      type="button"
-                      onClick={() => {
-                        this.setState({ hasCompletedTicket: true })
-                        this.createNewTicket();
-                      }}
-                      className="btn btn-submit"
+            <div className="autocomplete-field">
+              <label htmlFor="projects">Project</label><br></br>
+              <input
+                id="projects"
+                className="form-control"
+                disabled="disabled"
+                value={`${this.props.selectedProject['company.name']} — ${this.props.selectedProject['project.name']}`}
+              />
+            </div>
+            <div className="autocomplete-field">
+              <label htmlFor="phases">Phase</label><br></br>
+              <Autocomplete
+                id="phases"
+                items={this.state.phases}
+                getItemValue={item => item.path}
+                shouldItemRender={(item, value) => item.path.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                renderItem={(item, highlighted) =>
+                  <div key={`${item.phaseId}-${item.ticketId}`}>
+                    {item.path}
+                  </div>
+                }
+                value={this.state.phaseValue}
+                inputProps={{ className: "autocomplete-input form-control" }}
+                onChange={e => this.setState({ phaseValue: e.target.value })}
+                onSelect={value => {
+                  this.setState({
+                    phaseValue: value,
+                    selectedPhase: this.state.phases.filter(phase => phase.path === value),
+                  })
+                }}
+              />
+            </div>
+            {this.state.phaseValue && (
+              <div>
+                <label htmlFor="summary">Summary</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="summary"
+                  onChange={(e) => this.setState({ summary: e.target.value })}
+                  required
+                  autoComplete="off"
+                ></input>
+              </div>
+            )}
+            {this.state.summary && (
+              <div>
+                <label htmlFor="budget-hours">Budget Hours</label>
+                <input
+                  type="number"
+                  id="budget-hours"
+                  className="form-control"
+                  onChange={(e) => this.setState({ budget: e.target.value })}
+                  required
+                  min="0"
+                  step="0.25"
+                  placeholder="1"
+                  autoComplete="off"
+                >
+                </input>
+                {this.state.budget > 10 && (<p>Warning: This is a higher than normal budget</p>)}
+              </div>
+            )}
+            {this.state.budget > 0 && (
+              <div>
+                <label htmlFor="initial-description">Description</label>
+                <textarea
+                  id="initial-description"
+                  rows="4"
+                  cols="50"
+                  className="form-control"
+                  placeholder="This is optional"
+                  onChange={(e) => e.target.value.length > 5 && this.setState({ initialDescription: e.target.value })}
+                >
+                </textarea>
+              </div>
+            )}
+            {(this.state.summary && this.state.budget) && (
+              <button
+                type="button"
+                onClick={() => {
+                  this.setState({ hasCompletedTicket: true })
+                  this.createNewTicket();
+                }}
+                className="btn btn-submit"
+              >
+                Create Ticket
+              </button>
+            )}
+            {(this.state.hasCompletedTicket && (
+              <div>
+                <p>You've created a new ticket:
+                  {this.state.newTicketId && (
+                    <a
+                      href={process.env.REACT_APP_CONNECTWISE_SERVER_URL + `/services/system_io/Service/fv_sr100_request.rails?service_recid=&${this.state.newTicketId}companyName=sd`}
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      Create Ticket
-                    </button>
+                      {` #${this.state.newTicketId}`}
+                    </a>
                   )}
-                  {(this.state.hasCompletedTicket && (
-                    <div>
-                      <p>You've created a new ticket:
-                        {this.state.newTicketId && (
-                          <a
-                            href={process.env.REACT_APP_CONNECTWISE_SERVER_URL + `/services/system_io/Service/fv_sr100_request.rails?service_recid=&${this.state.newTicketId}companyName=sd`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            {` #${this.state.newTicketId}`}
-                          </a>
-                        )}
-                      </p>
-                      <button type="button" onClick={() => this.resetTicketDetails()}>Create another ticket</button>
-                    </div>
-                  ))}
-              </React.Fragment>
+                </p>
+                <button type="button" className="btn btn-default" onClick={() => this.resetTicketDetails()}>Create another ticket</button>
+              </div>
+            ))}
           </form>
         </Modal>
       </div>
