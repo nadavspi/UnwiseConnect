21a551ab6b38589090bf136d418b549a9b934420
Kyle Leighton <kyle.leighton1@gmail.com>
2021-01-07 17:26:18 -0500

    Add missing import and fix render() location

--- src/components/Tickets/CreateTicket.js
+++ src/components/Tickets/CreateTicket.js
@@ -1,143 +1,144 @@
 import React, { PureComponent } from 'react';
 import Autocomplete from 'react-autocomplete';
 import Modal from 'react-modal';
+import { createTicket } from '../../helpers/cw';
 
 class CreateTicket extends PureComponent {
-  render() {
-    emptyTicketState = {
-      budget: '',
-      description: '',
-      expanded: false,
-      hasCompletedTicket: false,
-      initialDescription: '',
-      newTicketId: '',
-      phases: [],
-      phaseValue: '',
-      projects: [],
-      projectValue: '',
-      selectedPhase: {},
-      selectedProject: {},
-      summary: '',
-      ticketType: 'project',
+  emptyTicketState = {
+    budget: '',
+    description: '',
+    expanded: false,
+    hasCompletedTicket: false,
+    initialDescription: '',
+    newTicketId: '',
+    phases: [],
+    phaseValue: '',
+    projects: [],
+    projectValue: '',
+    selectedPhase: {},
+    selectedProject: {},
+    summary: '',
+    ticketType: 'project',
+  }
+
+  state = {
+    ...this.emptyTicketState
+  }
+
+  componentDidMount = () => {
+    this.getProjects();
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.projects !== this.props.projects) {
+      this.getProjects();      
     }
 
-    state = {
-      ...this.emptyTicketState
-    }
-
-    componentDidMount = () => {
-      this.getProjects();
-    }
-
-	  componentDidUpdate = (prevProps) => {
-      if (prevProps.projects !== this.props.projects) {
-        this.getProjects();      
-      }
-
-      if (prevProps.selectedProject !== this.props.selectedProject) {
-        const { selectedProject } = this.props;
-        if (selectedProject['project.name']) {
-          this.getPhases(this.state.projects.filter(project => (
-            project.name === selectedProject['project.name'] &&
-            project.company.name === selectedProject['project.company']
-          )));
-
-          this.setState({
-            selectedProject: this.state.projects.filter(project => (
-              project.name === `${selectedProject['company.name']} — ${selectedProject['project.name']}`)
-            )
-          });
-        }
-      }
-    }
-
-    getPhases = () => {
-      let phases = [];
+    if (prevProps.selectedProject !== this.props.selectedProject) {
       const { selectedProject } = this.props;
+      if (selectedProject['project.name']) {
+        this.getPhases(this.state.projects.filter(project => (
+          project.name === selectedProject['project.name'] &&
+          project.company.name === selectedProject['project.company']
+        )));
 
-      this.props.tickets.map(ticket => {
-        if (selectedProject['project.name'] === ticket.project.name && selectedProject['company.name'] === ticket.company.name) {
-          phases.push({
-            path: ticket.phase.path,
-            phaseId: ticket.phase.id,
-            ticketId: ticket.id
-          });
-        }
-      });
-
-      this.setState({
-        phases
-      });
-    }
-
-    getProjects = () => {
-      let projects = [];
-
-      this.props.projects.map(project => {
-        projects.push({
-          name: `${project.company.name} — ${project.project.name}`,
-          id: project.project.id,
-          companyId: project.company.id
+        this.setState({
+          selectedProject: this.state.projects.filter(project => (
+            project.name === `${selectedProject['company.name']} — ${selectedProject['project.name']}`)
+          )
         });
-      });
-
-      this.setState({
-        projects
-      });
+      }
     }
+  }
 
-    createNewTicket = () => {
-      const projectTicketDetails = ({
-        summary: this.state.summary,
-        recordType: 'ProjectTicket',
-        company: { id: this.state.selectedProject[0].companyId },
-        project: { id: this.state.selectedProject[0].id },
-        phase: { id: this.state.selectedPhase[0].phaseId },
-        budgetHours: this.state.budget,
-        initialDescription: this.state.initialDescription,
-      })
+  getPhases = () => {
+    let phases = [];
+    const { selectedProject } = this.props;
 
-      // @todo allow service ticket creation
-      const serviceTicketDetails = ({
-        summary: this.state.summary,
-        recordType: 'ServiceTicket',
-        company: { id: this.state.selectedProject[0].companyId },
-        agreement: '', // where is this?
-        budgetHours: this.state.budget,
-        initialDescription: this.state.initialDescription,
-      })
+    this.props.tickets.map(ticket => {
+      if (selectedProject['project.name'] === ticket.project.name && selectedProject['company.name'] === ticket.company.name) {
+        phases.push({
+          path: ticket.phase.path,
+          phaseId: ticket.phase.id,
+          ticketId: ticket.id
+        });
+      }
+    });
 
-      if (this.state.ticketType === 'project') {
-        createTicket(projectTicketDetails).then(res => {
-          this.setState({
-            newTicketId: res.result.id
-          })
+    this.setState({
+      phases
+    });
+  }
+
+  getProjects = () => {
+    let projects = [];
+
+    this.props.projects.map(project => {
+      projects.push({
+        name: `${project.company.name} — ${project.project.name}`,
+        id: project.project.id,
+        companyId: project.company.id
+      });
+    });
+
+    this.setState({
+      projects
+    });
+  }
+
+  createNewTicket = () => {
+    const projectTicketDetails = ({
+      summary: this.state.summary,
+      recordType: 'ProjectTicket',
+      company: { id: this.state.selectedProject[0].companyId },
+      project: { id: this.state.selectedProject[0].id },
+      phase: { id: this.state.selectedPhase[0].phaseId },
+      budgetHours: this.state.budget,
+      initialDescription: this.state.initialDescription,
+    })
+
+    // @todo allow service ticket creation
+    const serviceTicketDetails = ({
+      summary: this.state.summary,
+      recordType: 'ServiceTicket',
+      company: { id: this.state.selectedProject[0].companyId },
+      agreement: '', // where is this?
+      budgetHours: this.state.budget,
+      initialDescription: this.state.initialDescription,
+    })
+
+    if (this.state.ticketType === 'project') {
+      createTicket(projectTicketDetails).then(res => {
+        this.setState({
+          newTicketId: res.result.id
         })
-      }
-
-      if (this.state.ticketType === 'service') {
-        createTicket(serviceTicketDetails);
-      }
-    };
-
-    expandAddTicketForm = () => {
-      this.setState({
-        expanded: !this.state.expanded
       })
     }
 
-    selectTicketType = event => {
-      this.setState({
-        ticketType: event.target.value
-      })
+    if (this.state.ticketType === 'service') {
+      createTicket(serviceTicketDetails);
     }
+  };
 
-    resetTicketDetails = () => {
-      this.setState({
-        ...this.emptyTicketState,
-      });
-    }
+  expandAddTicketForm = () => {
+    this.setState({
+      expanded: !this.state.expanded
+    })
+  }
 
+  selectTicketType = event => {
+    this.setState({
+      ticketType: event.target.value
+    })
+  }
+
+  resetTicketDetails = () => {
+    this.setState({
+      ...this.emptyTicketState,
+    });
+  }
+
+  render() {
     return (
       <div className="create-ticket-form">
         {this.props.selectedProject['company.name'] && (
