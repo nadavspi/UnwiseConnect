84c5805924456de2dbd528906392c0d0ff79de47
Kyle Leighton <kyle.leighton1@gmail.com>
2021-01-07 16:57:29 -0500

    Get the active projects on mount
    These are only the projects the user has selected; not all projects within Unwise

--- src/components/Tickets/CreateTicket.js
+++ src/components/Tickets/CreateTicket.js
@@ -23,6 +23,51 @@ class CreateTicket extends PureComponent {
       ...this.emptyTicketState
     }
 
+    componentDidMount = () => {
+      this.getProjects();
+    }
+
+    componentDidUpdate = (prevProps) => {
+      if (prevProps.projects !== this.props.projects) {
+        this.getProjects();      
+      }
+    }
+
+    getPhases = () => {
+      let phases = [];
+      const { selectedProject } = this.props;
+
+      this.props.tickets.map(ticket => {
+        if (selectedProject['project.name'] === ticket.project.name && selectedProject['company.name'] === ticket.company.name) {
+          phases.push({
+            path: ticket.phase.path,
+            phaseId: ticket.phase.id,
+            ticketId: ticket.id
+          });
+        }
+      });
+
+      this.setState({
+        phases
+      });
+    }
+
+    getProjects = () => {
+      let projects = [];
+
+      this.props.projects.map(project => {
+        projects.push({
+          name: `${project.company.name} — ${project.project.name}`,
+          id: project.project.id,
+          companyId: project.company.id
+        });
+      });
+
+      this.setState({
+        projects
+      });
+    }
+
     return (
       <div>
 
