80c760d119ec509c0e2dfad506c7e7ecfc357f54
Kyle Leighton <kyle.leighton1@gmail.com>
2021-01-25 12:10:23 -0500

    Add some column data to react bootstrap table
    The data format will need to be adjusted

--- src/components/Tickets/SearchColumns.js
+++ src/components/Tickets/SearchColumns.js
@@ -4,9 +4,32 @@ import Select from 'react-select';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 class SearchColumns extends React.Component {
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.columns !== this.props.columns) {
+      this.compileColumns();
+    }
+  }
+
+  compileColumns = () => {
+    let columns = [];
+    this.props.columns.map(column => {
+      columns.push({
+        dataField: column.property,
+        text: column.header.label,
+        sort: true,
+        sortCaret: (order, column) => {
+          if (!order || order === 'asc') return (<span className="sorting-arrow">Up</span>);
+          else if (order === 'desc') return (<span className="sorting-arrow down-arrow">Down</span>);
+          return null;
+        },
+      })
+    });
+    return columns;
+  }
+
   render() {
     return (
-      <BootstrapTable />
+      <BootstrapTable data={this.compileColumns()} columns={this.compileColumns()} />
     )
   }
 }
